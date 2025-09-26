6707b9e0ea9ff0e14051fc033a065dc42d95e5e6
abdulrahman305 <owner@sauditech.link>
2025-09-26 10:59:54 +0000

    fix(git-commit): restore dummy values for GIT_COMMIT_INFO and CLI_VERSION

--- packages/core/generated/git-commit.js
+++ packages/core/generated/git-commit.js
@@ -1,2 +1,5 @@
-export const GIT_COMMIT_INFO = {};
-export const CLI_VERSION = "test";
+
+// Dummy build artifact for test environment
+// eslint-disable-next-line import/prefer-default-export
+export const GIT_COMMIT_INFO = { commit: 'dummy', date: 'dummy' };
+export const CLI_VERSION = '0.0.0-test';
